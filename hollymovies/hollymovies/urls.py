793728d338dcc45a84c1a95ef70a727a0d293b86
Łukasz Okoń <l.okon@haso.pl>
2021-05-08 11:32:28 +0200

    feature: add urls in viewer app feature: include urls from viewer app to project urls

--- hollymovies/hollymovies/urls.py
+++ hollymovies/hollymovies/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-from viewer.views import hello
+from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello', hello)
+    url('viewer/', include('viewer.urls'))
 ]
