61e9a0230f5ebc738d0aea2a0fe222365cea28be
Łukasz Okoń <l.okon@haso.pl>
2021-05-08 12:16:35 +0200

    feature: add Movie model in viewer app

--- hollymovies/viewer/models.py
+++ hollymovies/viewer/models.py
@@ -1,7 +1,18 @@
-from django.db.models import CharField, Model
+from django.db.models import CharField, Model, ForeignKey, DateTimeField, DateField, \
+    IntegerField, TextField, DO_NOTHING
+
 
 # Create your models here.
 
 
 class Genre(Model):
     name = CharField(max_length=128)
+
+
+class Movie(Model):
+    title = CharField(max_length=128)
+    genre = ForeignKey(Genre, on_delete=DO_NOTHING)
+    rating = IntegerField()
+    released = DateField()
+    description = TextField()
+    created = DateTimeField(auto_now_add=True)
