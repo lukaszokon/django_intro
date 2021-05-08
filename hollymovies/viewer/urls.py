08e52410335cc8872d4a006f04ac7665b5fe4a98
Łukasz Okoń <l.okon@haso.pl>
2021-05-08 15:37:32 +0200

    feature: add base, hello, movies templates feature: add views for added templates feature: add urls for added templates

--- hollymovies/viewer/urls.py
+++ hollymovies/viewer/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import hello
+from .views import hello, hello_template, movies
 
 
 urlpatterns = [
-    path('hello', hello)
+    path('hello', hello),
+    path('movies', movies, name='movies'),
+    path('', hello_template, name='home'),
 ]
--- hollymovies/viewer/views.py
+++ hollymovies/viewer/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 # Create your views here.
 
@@ -7,3 +8,12 @@ from django.http import HttpResponse
 def hello(request):
     s = request.GET.get('s', '')
     return HttpResponse(f'Hello {s} World!')
+
+
+def hello_template(request):
+    s1 = request.GET.get('s1', '')
+    return render(request, template_name='hello.html', context={'adjectives': [s1, 'piękny', 'cudowny']})
+
+
+def movies(request):
+    return render(request, template_name='movies.html', context={'movies': Movie.objects.all()})
