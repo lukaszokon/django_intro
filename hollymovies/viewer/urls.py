069c72fa5ef01c73d824248437af2bb6ec3ea9ce
Łukasz Okoń <l.okon@haso.pl>
2021-05-09 11:55:02 +0200

    feature: add filter options to MovieListView feature: add sorting options to MovieListView feature: add default sorting to GenreListView

--- hollymovies/viewer/urls.py
+++ hollymovies/viewer/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import hello, hello_template, movies, MovieView, MovieDetailView
+from .views import hello, hello_template, movies, MovieView, MovieDetailView, GenreListView
 
 
 urlpatterns = [
     path('hello', hello),
     path('movies/<int:pk>/', MovieDetailView.as_view(), name='movie-detail'),
     path('movies', MovieView.as_view(), name='movies'),
+    path('genre', GenreListView.as_view(), name='genres'),
     path('', hello_template, name='home'),
 ]
--- hollymovies/viewer/views.py
+++ hollymovies/viewer/views.py
@@ -1,20 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Movie
+from .models import Movie, Genre
 from django.views.generic import ListView, DetailView
-
-
-# Create your views here.
-
-class MovieDetailView(DetailView):
-    model = Movie
-    template_name = 'movie_detail.html'
+from operator import attrgetter
 
 
 class MovieView(ListView):
     template_name = 'movies.html'
     model = Movie
 
+    def get_queryset(self):
+        genre = self.request.GET.get('genre')
+        sort = self.request.GET.get('sort')
+        if genre:
+            queryset = Movie.objects.filter(genre__name=genre)
+        else:
+            queryset = Movie.objects.all()
+        if sort:
+            queryset = sorted(queryset, key=attrgetter(sort))
+        return queryset
+
+
+class GenreListView(ListView):
+    template_name = 'genres.html'
+    model = Genre
+    ordering = 'name'
+
+
+class MovieDetailView(DetailView):
+    model = Movie
+    template_name = 'movie_detail.html'
 
 
 def hello(request):
